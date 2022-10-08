0779202da476ae7541940ae5613bda23ab210c08
Mang432 <72353782+Mang432@users.noreply.github.com>
2022-10-08 13:39:29 -0300

    update gitignore again

--- Main.cs
+++ Main.cs
@@ -11,89 +11,89 @@ using System.Security.Cryptography;
 [assembly: MelonGame("Dani", "Karlson")]
 namespace KarlsonLevels
 {
-    public class Main : MelonMod
-    {
-        bool initialized;
-        public static GameObject[] Prefabs;
-        public static List<LevelObject> Level = new List<LevelObject>();
-        public static LevelObjectData[] LevelData;
-        public static bool editMode;
-        public static MoveModeEnum MovementMode;
-        public static int movableObj; // Id of the object that's currently in control, only applicable in edit mode
-        public const string version = "0.1.3";
-        public override void OnSceneWasInitialized(int buildIndex, string sceneName) {
-            base.OnSceneWasInitialized(buildIndex, sceneName);
-            editMode = false;
-            if (buildIndex == 1 && !initialized)
-            {
-                initialized = true;
-                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Levels");
-                MelonCoroutines.Start(PrefabInitializer());
-            }
-        }
+	public class Main : MelonMod
+	{
+		bool initialized;
+		public static GameObject[] Prefabs;
+		public static List<LevelObject> Level = new List<LevelObject>();
+		public static LevelObjectData[] LevelData;
+		public static bool editMode;
+		public static MoveModeEnum MovementMode;
+		public static int movableObj; // Id of the object that's currently in control, only applicable in edit mode
+		public const string version = "0.1.3";
+		public override void OnSceneWasInitialized(int buildIndex, string sceneName) {
+			base.OnSceneWasInitialized(buildIndex, sceneName);
+			editMode = false;
+			if (buildIndex == 1 && !initialized)
+			{
+				initialized = true;
+				Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Levels");
+				MelonCoroutines.Start(PrefabInitializer());
+			}
+		}
 
-        IEnumerator PrefabInitializer() {
-            List<GameObject> objs = new List<GameObject>();
-            for (int i = 2; i <= 12; i++)
-            {
-                SceneManager.LoadScene(i);
-                yield return null;
-                foreach (Collider g in Object.FindObjectsOfType<Collider>())
-                {
-                    objs.Add(g.gameObject);
-                    g.gameObject.transform.parent = null;
-                    Object.DontDestroyOnLoad(g.gameObject);
-                    g.gameObject.SetActive(false);
-                    yield return null;
-                }
-                MelonLogger.Msg("Level " + (i - 1) + " loaded");
-            }
-            Prefabs = objs.ToArray();
-            SceneManager.LoadScene(1);
-            MelonLogger.Msg(Prefabs.Length + " objects loaded");
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
+		IEnumerator PrefabInitializer() {
+			List<GameObject> objs = new List<GameObject>();
+			for (int i = 2; i <= 12; i++)
+			{
+				SceneManager.LoadScene(i);
+				yield return null;
+				foreach (Collider g in Object.FindObjectsOfType<Collider>())
+				{
+					objs.Add(g.gameObject);
+					g.gameObject.transform.parent = null;
+					Object.DontDestroyOnLoad(g.gameObject);
+					g.gameObject.SetActive(false);
+					yield return null;
+				}
+				MelonLogger.Msg("Level " + (i - 1) + " loaded");
+			}
+			Prefabs = objs.ToArray();
+			SceneManager.LoadScene(1);
+			MelonLogger.Msg(Prefabs.Length + " objects loaded");
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
 
-        public static int IdToIndex(int Id) {
-            for (int i = 0; i < Level.Count; i++)
-            {
-                if (Level[i].Id == Id) return i;
-            }
-            return -1;
-        }
+		public static int IdToIndex(int Id) {
+			for (int i = 0; i < Level.Count; i++)
+			{
+				if (Level[i].Id == Id) return i;
+			}
+			return -1;
+		}
 
-        public static Vector3 StringToVector3(string sVector) {
-            // Remove the parentheses
-            if (sVector.StartsWith("(") && sVector.EndsWith(")"))
-            {
-                sVector = sVector.Substring(1, sVector.Length - 2);
-            }
+		public static Vector3 StringToVector3(string sVector) {
+			// Remove the parentheses
+			if (sVector.StartsWith("(") && sVector.EndsWith(")"))
+			{
+				sVector = sVector.Substring(1, sVector.Length - 2);
+			}
 
-            // split the items
-            string[] sArray = sVector.Split(',');
+			// split the items
+			string[] sArray = sVector.Split(',');
 
-            // store as a Vector3
-            Vector3 result = new Vector3(
-                float.Parse(sArray[0]),
-                float.Parse(sArray[1]),
-                float.Parse(sArray[2]));
+			// store as a Vector3
+			Vector3 result = new Vector3(
+				float.Parse(sArray[0]),
+				float.Parse(sArray[1]),
+				float.Parse(sArray[2]));
 
-            return result;
-        }
+			return result;
+		}
 
-        public enum MoveModeEnum : byte { movement, scale, rotation }
+		public enum MoveModeEnum : byte { movement, scale, rotation }
 
-        public struct LevelObject
-        {
-            public GameObject Object;
-            public int Id, prefab;
-        }
+		public struct LevelObject
+		{
+			public GameObject Object;
+			public int Id, prefab;
+		}
 
-        public struct LevelObjectData
-        {
-            public Vector3 scale, position, rotation;
-            public int Id, prefab;
-        }
-    }
+		public struct LevelObjectData
+		{
+			public Vector3 scale, position, rotation;
+			public int Id, prefab;
+		}
+	}
 }
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -9,28 +9,28 @@ using UnityEngine;
 
 namespace KarlsonLevels
 {
-    [HarmonyPatch(typeof(PlayerMovement), "Movement")]
-    class PlayerMovementPatch
-    {
-        static bool Prefix(PlayerMovement __instance) {
-            if (Main.editMode)
-            {
-                float x = Input.GetAxisRaw("Horizontal");
-                float z = Input.GetAxisRaw("Vertical");
-                float y = __instance.gameObject.transform.position.y;
-                if (Main.movableObj == 0)
-                {
-                    __instance.gameObject.transform.position += Camera.main.transform.forward * z + Camera.main.transform.right * x;
-                    __instance.rb.velocity = Vector3.zero;
-                }
-                else
-                {
-                    Vector3 foo = Camera.main.transform.forward * z + Camera.main.transform.right * x;
-                    Main.Level[Main.IdToIndex(Main.movableObj)].Object.transform.position += new Vector3(foo.x, 0, foo.z);
-                }
-                return false;
-            }
-            else return true;
-        }
-    }
+	[HarmonyPatch(typeof(PlayerMovement), "Movement")]
+	class PlayerMovementPatch
+	{
+		static bool Prefix(PlayerMovement __instance) {
+			if (Main.editMode)
+			{
+				float x = Input.GetAxisRaw("Horizontal");
+				float z = Input.GetAxisRaw("Vertical");
+				float y = __instance.gameObject.transform.position.y;
+				if (Main.movableObj == 0)
+				{
+					__instance.gameObject.transform.position += Camera.main.transform.forward * z + Camera.main.transform.right * x;
+					__instance.rb.velocity = Vector3.zero;
+				}
+				else
+				{
+					Vector3 foo = Camera.main.transform.forward * z + Camera.main.transform.right * x;
+					Main.Level[Main.IdToIndex(Main.movableObj)].Object.transform.position += new Vector3(foo.x, 0, foo.z);
+				}
+				return false;
+			}
+			else return true;
+		}
+	}
 }
